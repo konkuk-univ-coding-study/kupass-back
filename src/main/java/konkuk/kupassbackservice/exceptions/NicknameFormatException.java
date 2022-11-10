cda4170f67700687bedb266bda9ea97033e7fc47
kyuhyeongkwon <akcps1tp@naver.com>
2022-11-10 22:49:59 +0900

    feat: kupass back service exceptions

---
+++ src/main/java/konkuk/kupassbackservice/exceptions/NicknameFormatException.java
@@ -0,0 +1,18 @@
+package konkuk.kupassbackservice.exceptions;
+
+public class NicknameFormatException extends IllegalArgumentException {
+    public NicknameFormatException() {
+    }
+
+    public NicknameFormatException(String s) {
+        super(s);
+    }
+
+    public NicknameFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NicknameFormatException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/konkuk/kupassbackservice/exceptions/PasswordFormatException.java
@@ -0,0 +1,18 @@
+package konkuk.kupassbackservice.exceptions;
+
+public class PasswordFormatException extends IllegalArgumentException {
+    public PasswordFormatException() {
+    }
+
+    public PasswordFormatException(String s) {
+        super(s);
+    }
+
+    public PasswordFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PasswordFormatException(Throwable cause) {
+        super(cause);
+    }
+}
