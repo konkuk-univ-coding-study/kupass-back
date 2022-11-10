c8665da6204689d951835336e183c340fdd7d73f
kyuhyeongkwon <akcps1tp@naver.com>
2022-11-10 22:46:58 +0900

    feat: kupass back service domain

---
+++ src/main/java/konkuk/kupassbackservice/domain/Article.java
@@ -0,0 +1,40 @@
+package konkuk.kupassbackservice.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "article_id")
+    private Long id;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+
+    private String publisher;
+
+    private String category;
+
+    private String title;
+
+    private String content;
+
+    private String summary;
+
+    private String source;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
+    private List<ArticleKeywords> keywords = new ArrayList<>();
+}
---
+++ src/main/java/konkuk/kupassbackservice/domain/Keyword.java
@@ -0,0 +1,20 @@
+package konkuk.kupassbackservice.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Keyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "keyword_id")
+    private Long id;
+
+    private String keyword;
+
+}
